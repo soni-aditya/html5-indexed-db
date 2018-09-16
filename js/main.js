4b99fb5a612cf6321fd0854ff74ba11995d7f613
Aditya Soni <adityas@geekyants.com>
2018-09-16 09:07:49 +0530

    feature: Adding customers to our indexedDB.

--- js/main.js
+++ js/main.js
@@ -11,14 +11,14 @@ Each time we want to update our DB, we have to use a different version number [T
  */
 $(document).ready(function() {
   // Let us open our database
-  var request = indexedDB.open("CustomerManager", 1);
+  var request = indexedDB.open("CustomerManager", 3);
   //Upgrading/Creating DB
   request.onupgradeneeded = function(e) {
     var db = e.target.result;
     //Check if there is one already created
-    //For this we just look for names of customers
-    if (!db.objectStoreNames.contains("customers")) {
-      var objectStore = db.createObjectStore("customers", {
+    //For this we just look for names of people
+    if (!db.objectStoreNames.contains("customer")) {
+      var objectStore = db.createObjectStore("customer", {
         keypath: "id",
         autoIncrement: true
       });
@@ -28,11 +28,37 @@ $(document).ready(function() {
   request.onsuccess = function(e) {
     console.log("BD Connected and Opened Successfully");
     db = e.target.result;
-    //Show Customers
-    showCustomers();
+    //Show people
+    // showpeople();
   };
   //Error
   request.onerror = function(e) {
     console.log("Some Error while Opening DB");
   };
 });
+
+function addCustomer() {
+  //Fetching info from the form
+  var name = $("#customer-name").val();
+  var email = $("#customer-email").val();
+  //Enter Name of objectStore in transaction
+  var transaction = db.transaction(["customer"], "readwrite");
+  //Ask for the objectStore
+  var store = transaction.objectStore("customer");
+  //Define a customer that we want to enter
+  var customer = {
+    name: name,
+    email: email
+  };
+  //Perform the add
+  var request = store.add(customer);
+  //Success-Error callbacks
+  request.onsuccess = function(e) {
+    console.log("Customer Saved Successfully");
+    window.location.href = "index.html";
+  };
+  request.onerror = function(e) {
+    alert("User isn't added\n Sorry !");
+    console.log("Error", e.target.error.name);
+  };
+}
